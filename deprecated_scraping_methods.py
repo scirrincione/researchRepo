520510cd18dc59a079f60f159c90aade7650ec43
Sofia Cirrincione <cirri006@umn.edu>
2025-02-18 15:47:02 -0600

    separated out deprecated method & retrieve handle to check if correct tweet

---
+++ deprecated_scraping_methods.py
@@ -0,0 +1,89 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import csv
+import os
+import pandas as pd
+import time
+import re
+import xlsxwriter
+from datetime import datetime
+
+def get_csv():
+    driver=webdriver.Chrome()
+    if os.path.exists("tweets.csv"):
+        os.remove("tweets.csv")
+    with open("tweets.csv", "w", newline = "") as pf:
+        writer = csv.writer(pf)
+        writer.writerow(["ID", "main_tweet", "reply_list"])
+        with open("tweet_ids.csv", "r") as f:
+            lines = f.readlines()
+            for line in lines[1:]:
+                if line == None or "No tweet found" in line:
+                    continue
+                else:
+                    id = line.rsplit(',',1)[-1]
+                    print(id)
+                    target_url = "https://nitter.net/anyuser/status/"+id
+
+                    driver.get(target_url)
+
+                    resp = driver.page_source
+                    
+
+                    soup = BeautifulSoup(resp, 'html.parser')
+
+            
+                    tweet = soup.find("div", id="m")
+                    cleaned_tweet = [line.strip() for line in tweet.text.split("\n") if line.strip()]
+                    replies = soup.find_all(attrs={'class':'reply thread thread-line'})
+                    cleaned_replies = [[line.strip() for line in reply.text.split("\n") if line.strip()] for reply in replies]
+                    print(cleaned_replies)
+                    extract_replies = [reply[4] for reply in cleaned_replies]
+                    writer.writerow([id,cleaned_tweet[3],extract_replies])
+    driver.close()
+
+def getTweetIDs():
+    driver=webdriver.Chrome()
+    if os.path.exists("tweet_ids.csv"):
+        os.remove("tweet_ids.csv")
+    with open("tweet_ids.csv", "w", newline = "") as pf:
+        writer = csv.writer(pf)
+        writer.writerow(["tweet_text", "tweet_id", "real_user_handle"])
+        df = pd.read_excel("Genuine_Disclosure_Posts/Australia_Posts.xlsx")
+        no_tweet_count = 0
+        for index, row in df.iterrows():
+            text = "\"" + row["tweet_text"] + "\""
+            text_formatted = text.replace(" ", "+")
+            text_formatted = text_formatted.replace("\n", "")
+            text_formatted = text_formatted.replace("@", "%40")
+            text_formatted = text_formatted.replace(",", "%2C")
+            text_formatted = text_formatted.replace("#", "%23")
+            text_formatted = text_formatted.replace("'", "%27")
+            text_formatted = text_formatted.replace(";", "%3B")
+            text_formatted = text_formatted.replace("&", "%26")
+            url = "https://nitter.net/search?f=tweets&q=" + text_formatted + "&since=&until=&near="
+            
+
+            driver.get(url)
+
+            resp = driver.page_source
+
+            soup = BeautifulSoup(resp, 'html.parser')
+            tweet = soup.find("div", attrs={'class':'tweet-link'})
+            tweet = soup.find("a", attrs={'class':'tweet-link'})
+            handle = soup.find("a", attrs={'class':'profile-card-username'})
+
+            if tweet is None:
+                writer.writerow([row["user_id"], "No tweet found "+str(no_tweet_count)])
+                no_tweet_count += 1
+            else:
+                writer.writerow([row["user_id"], extract_tweet_id_regex(tweet['href']), handle.text[1:]])
+    driver.close()
+
+def workbookTests():
+    workbook = xlsxwriter.Workbook('test.xlsx')
+    worksheet = workbook.add_worksheet()
+    df = pd.read_excel("Genuine_Disclosure_Posts/Australia_Posts.xlsx")
+    for index, row in df.iterrows():
+        worksheet.write_string(index, 0, row["tweet_text"])
+    workbook.close()
--- dog_scrape.py
+++ dog_scrape.py
@@ -17,6 +17,7 @@ def get_xlsx(sheet, sheet_name):
     sheet.write_string(rowC, 0, "ID")
     sheet.write_string(rowC, 1, "tweet")
     sheet.write_string(rowC, 2, "replies")
+    sheet.write_string(rowC, 3, "user_handle")
     rowC += 1
 
     for index,row in df.iterrows():
@@ -31,50 +32,19 @@ def get_xlsx(sheet, sheet_name):
             resp = driver.page_source
             time.sleep(2)
             soup = BeautifulSoup(resp, 'html.parser')
-    
-            replies = soup.find_all(attrs={'class':'reply thread thread-line'})
-            cleaned_replies = [[line.strip() for line in reply.text.split("\n") if line.strip()] for reply in replies]
-            extract_replies = [reply[4] if len(reply) > 4 else reply[-1] for reply in cleaned_replies]
-            
-            replies_string = "\t".join(extract_replies)
-            sheet.write_string(rowC, 0, id)
-            sheet.write_string(rowC, 1, row["tweet_text"])
-            sheet.write_string(rowC, 2, replies_string)
-            rowC += 1
-    driver.close()
-
-def get_csv():
-    driver=webdriver.Chrome()
-    if os.path.exists("tweets.csv"):
-        os.remove("tweets.csv")
-    with open("tweets.csv", "w", newline = "") as pf:
-        writer = csv.writer(pf)
-        writer.writerow(["ID", "main_tweet", "reply_list"])
-        with open("tweet_ids.csv", "r") as f:
-            lines = f.readlines()
-            for line in lines[1:]:
-                if line == None or "No tweet found" in line:
-                    continue
-                else:
-                    id = line.rsplit(',',1)[-1]
-                    print(id)
-                    target_url = "https://nitter.net/anyuser/status/"+id
-
-                    driver.get(target_url)
-
-                    resp = driver.page_source
-                    
-
-                    soup = BeautifulSoup(resp, 'html.parser')
-
-            
-                    tweet = soup.find("div", id="m")
-                    cleaned_tweet = [line.strip() for line in tweet.text.split("\n") if line.strip()]
-                    replies = soup.find_all(attrs={'class':'reply thread thread-line'})
-                    cleaned_replies = [[line.strip() for line in reply.text.split("\n") if line.strip()] for reply in replies]
-                    print(cleaned_replies)
-                    extract_replies = [reply[4] for reply in cleaned_replies]
-                    writer.writerow([id,cleaned_tweet[3],extract_replies])
+            handle = soup.find("a", attrs={'class':'username'})
+            if handle[1:] != row["user_handle"]:
+                continue
+            else:
+                replies = soup.find_all(attrs={'class':'reply thread thread-line'})
+                cleaned_replies = [[line.strip() for line in reply.text.split("\n") if line.strip()] for reply in replies]
+                extract_replies = [reply[4] if len(reply) > 4 else reply[-1] for reply in cleaned_replies]
+                
+                replies_string = "\t".join(extract_replies)
+                sheet.write_string(rowC, 0, id)
+                sheet.write_string(rowC, 1, row["tweet_text"])
+                sheet.write_string(rowC, 2, replies_string)
+                rowC += 1
     driver.close()
         
 def remove_links(text):
@@ -100,9 +70,10 @@ def getTweetIDs_xlsx(tweet_workbook, sheet, sheet_name):
     no_tweet_count = 0
     text_col = 0
     id_col = 1
-    date_col = 2
+    user_col = 2
     sheet.write_string(0, text_col, "tweet_text")
     sheet.write_string(0, id_col, "tweet_id")
+    sheet.write_string(0, user_col, "user_handle")
 
     for index, row in df.iterrows():
         text = "\"" + row["tweet_text"] + "\""
@@ -123,63 +94,21 @@ def getTweetIDs_xlsx(tweet_workbook, sheet, sheet_name):
         time.sleep(1)
         soup = BeautifulSoup(resp, 'html.parser')
         tweet = soup.find("a", attrs={'class':'tweet-link'})
+        handle = soup.find("a", attrs={'class':'profile-card-username'})
         #date = soup.find("div", attrs)
         if tweet is None:
             sheet.write_string(index+1, text_col, row["tweet_text"])
             no_tweet =  "No tweet found "+ str(no_tweet_count)
             sheet.write_string(index+1, id_col, no_tweet)
+            sheet.write_string(index+1, user_col, handle[1:])
             no_tweet_count += 1
         else:
             sheet.write_string(index+1, text_col, row["tweet_text"])
             sheet.write(index+1, id_col, extract_tweet_id_regex(tweet['href']))
+            sheet.write_string(index+1, user_col, handle[1:])
             
         
     
-    driver.close()
-
-def workbookTests():
-    workbook = xlsxwriter.Workbook('test.xlsx')
-    worksheet = workbook.add_worksheet()
-    df = pd.read_excel("Genuine_Disclosure_Posts/Australia_Posts.xlsx")
-    for index, row in df.iterrows():
-        worksheet.write_string(index, 0, row["tweet_text"])
-    workbook.close()
-
-def getTweetIDs():
-    driver=webdriver.Chrome()
-    if os.path.exists("tweet_ids.csv"):
-        os.remove("tweet_ids.csv")
-    with open("tweet_ids.csv", "w", newline = "") as pf:
-        writer = csv.writer(pf)
-        writer.writerow(["tweet_text", "tweet_id"])
-        df = pd.read_excel("Genuine_Disclosure_Posts/Australia_Posts.xlsx")
-        no_tweet_count = 0
-        for index, row in df.iterrows():
-            text = "\"" + row["tweet_text"] + "\""
-            text_formatted = text.replace(" ", "+")
-            text_formatted = text_formatted.replace("\n", "")
-            text_formatted = text_formatted.replace("@", "%40")
-            text_formatted = text_formatted.replace(",", "%2C")
-            text_formatted = text_formatted.replace("#", "%23")
-            text_formatted = text_formatted.replace("'", "%27")
-            text_formatted = text_formatted.replace(";", "%3B")
-            text_formatted = text_formatted.replace("&", "%26")
-            url = "https://nitter.net/search?f=tweets&q=" + text_formatted + "&since=&until=&near="
-            
-
-            driver.get(url)
-
-            resp = driver.page_source
-
-            soup = BeautifulSoup(resp, 'html.parser')
-            tweet = soup.find("div", attrs={'class':'tweet-link'})
-            tweet = soup.find("a", attrs={'class':'tweet-link'})
-
-            if tweet is None:
-                writer.writerow([row["user_id"], "No tweet found "+str(no_tweet_count)])
-                no_tweet_count += 1
-            else:
-                writer.writerow([row["user_id"], extract_tweet_id_regex(tweet['href'])])
     driver.close()
 
 def extract_tweet_id_regex(html_string):
@@ -226,6 +155,14 @@ def getUserHandle(userID):
 
 def getUserHandleTweet(tweetID):
     driver=webdriver.Chrome()
+    url = "https://nitter.net/anyuser/status/"+tweetID
+    driver.get(url)
+    resp = driver.page_source
+    time.sleep(2)
+    driver.close()
+    soup = BeautifulSoup(resp, 'html.parser')
+    handle = soup.find("a", attrs={'class':'username'})
+    print(handle.text[1:])
 
 def get_all_ids():
     tweet_workbook = xlsxwriter.Workbook("new_tweet_ids.xlsx")
@@ -243,4 +180,4 @@ def get_all_replies():
         get_xlsx(wsheet, sheet)
     tweet_workbook.close()
 
-getUserHandle("2609400548")
+getUserHandleTweet("1575965103144697856")
