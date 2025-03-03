04268d2865afb205c8629f7e1c2f22e6784dbedf
Sofia Cirrincione <cirri006@umn.edu>
2025-03-03 10:33:18 -0600

    fixing repeats in data

--- data_analysis/data_analysis.py
+++ data_analysis/data_analysis.py
@@ -12,30 +12,45 @@ def get_averages():
     av_data = pd.ExcelWriter('replies_averages.xlsx')
 
     for sheet_name in data.sheet_names:
-        sheet = pd.read_excel('../data_collection/tuple_tweet_replies.xlsx', sheet_name)
-        comments = 0
-        retweets = 0
-        quotes = 0
-        likes = 0
-        reply_vals = {'comment_count': 0, 'retweet_count': 0,'quote_count': 0,'heart_count': 0}
+        sheet = pd.read_excel(data, sheet_name)
+        rep_df = pd.DataFrame(columns = ['comment_count', 'retweet_count', 'quote_count', 'heart_count', 'tweet_body', 'handle'])
         
         for index,row in sheet.iterrows():
             if index == 1:
                 continue
-            comments += row['comment_number']
-            retweets += row['retweet_number']
-            quotes += row['quote_number']
-            likes += row['like_number']
+           
             replies = ast.literal_eval(row['replies'])
+            rep_df = pd.DataFrame(columns = ['comment_count', 'retweet_count', 'quote_count', 'heart_count', 'tweet_body', 'handle'])
+            count = 0
             for key,value in replies:
-                if key in reply_vals:
-                    reply_vals[key] += int(value)
-                        
+                rep_df.loc[count, key]=value 
+                if(key == 'handle'):
+                    count+=1
+            rep_df.drop_duplicates(subset=['tweet_body'], inplace=True)
+            sheet.at[index,'replies'] = rep_df.to_string()
         l = len(sheet)
-        av_reply_vals = [value/comments for value in reply_vals.values()]
-        sheet.loc[l]=([0, 'AVERAGES', '', comments/l, retweets/l, quotes/l, likes/l, str(av_reply_vals)])
+        av_reply_vals = rep_df.mean(numeric_only=True)
+        av_tweet_vals = sheet.mean(numeric_only=True)
+        sum_reply_vals = rep_df.sum(numeric_only=True)
+        sum_tweet_vals = sheet.sum(numeric_only=True)
+        sheet.loc[l]=([0, 'AVERAGES', '', av_tweet_vals['comment_number'], av_tweet_vals['retweet_number'], av_tweet_vals['quote_number'], av_tweet_vals['like_number'], av_reply_vals.to_string()])
+        sheet.loc[l+1]=([0, 'SUMS', '', sum_tweet_vals['comment_number'], sum_tweet_vals['retweet_number'], sum_tweet_vals['quote_number'], sum_tweet_vals['like_number'], sum_reply_vals.to_string()])
         sheet.to_excel(av_data, sheet_name=sheet_name)
     av_data.close()
 
-get_averages()
+def get_tweet_csv():
+    data = pd.ExcelFile('../data_collection/tuple_tweet_replies.xlsx')
+    replies = []
+    for sheet_name in data.sheet_names:
+        sheet = pd.read_excel(data, sheet_name)
+        for index, row in sheet.iterrows():
+            rep = pd.read_csv(row['replies'])
+            
+            replies.append(rep['tweet_body'])
+
+    df = pd.DataFrame(replies, columns=['tweet_body'])
+    df.to_csv("tweet_replies.csv")
+
+
+get_tweet_csv()
 
